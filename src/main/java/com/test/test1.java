b6451c1e8c6825f2bd9011d33fd34ab02a472162
xiaosongbro <137157837+xiaosongbro@users.noreply.github.com>
2024-02-29 15:45:43 +0800

    Update test1.java

--- src/main/java/com/test/test1.java
+++ src/main/java/com/test/test1.java
@@ -6,5 +6,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("haha");
         System.out.println("push");
+         System.out.println("poll");
     }
 }
